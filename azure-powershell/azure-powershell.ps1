d4b3cc7123c15a8b8a0372ab0f2f2078914a178e
jlee-consulting <36266460+jlee-consulting@users.noreply.github.com>
2021-10-13 00:03:30 -0400

    Add mainTemplate.json

--- azure-powershell/azure-powershell.ps1
+++ azure-powershell/azure-powershell.ps1
@@ -1,5 +1,7 @@
 param([string] $name)
-$output = 'Hello {0}' -f $name
+
+$output = 'Hello, {0}' -f $name
 Write-Output $output
+
 $DeploymentScriptOutputs = @{}
 $DeploymentScriptOutputs['text'] = $output
