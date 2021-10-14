c922ebfcd0433b63e85c5d64f0642ceba84e8b08
jlee-consulting <36266460+jlee-consulting@users.noreply.github.com>
2021-10-14 01:57:34 -0400

    Update azure-powershell-v5.ps1

--- azure-powershell/azure-powershell-v5.ps1
+++ azure-powershell/azure-powershell-v5.ps1
@@ -6,8 +6,11 @@ Write-Output $output
 ## WARNING: Interactive authentication is not supported in this session, please run cmdlet 'Connect-AzAccount -UseDeviceAuthentication'.
 # Connect-AzAccount -Subscription $subscriptionId
 
-## The following seems to hang when running inside the deploymentScripts context.
+## Connect-AzAccount -UseDeviceAuthentication cannot be used inside the deploymentScripts context.
+## DeviceCodeCredential authentication failed: Verification code expired before contacting the server
 # Connect-AzAccount -UseDeviceAuthentication
+
+Get-AzResourceGroup
 
 $DeploymentScriptOutputs = @{}
 $DeploymentScriptOutputs['text'] = $output
