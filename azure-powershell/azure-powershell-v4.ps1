e8d092638433b02009382adf241fa156a65f7c29
jlee-consulting <36266460+jlee-consulting@users.noreply.github.com>
2021-10-14 00:54:14 -0400

    Add v4

---
+++ azure-powershell/azure-powershell-v4.ps1
@@ -0,0 +1,9 @@
+param([string] $subscriptionId)
+
+$output = 'subscriptionId: {0}' -f $subscriptionId
+Write-Output $output
+
+Connect-AzAccount -Subscription $subscriptionId
+
+$DeploymentScriptOutputs = @{}
+$DeploymentScriptOutputs['text'] = $output
