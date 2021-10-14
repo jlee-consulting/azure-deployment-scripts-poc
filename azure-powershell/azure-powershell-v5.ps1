044953a529ea8d51834ad214d73311664877624c
jlee-consulting <36266460+jlee-consulting@users.noreply.github.com>
2021-10-14 01:45:17 -0400

    Add v5

--- azure-powershell/azure-powershell-v5.ps1
+++ azure-powershell/azure-powershell-v5.ps1
@@ -6,8 +6,8 @@ Write-Output $output
 ## WARNING: Interactive authentication is not supported in this session, please run cmdlet 'Connect-AzAccount -UseDeviceAuthentication'.
 # Connect-AzAccount -Subscription $subscriptionId
 
-# The following seems to hang when running inside the deploymentScripts context.
-Connect-AzAccount -UseDeviceAuthentication
+## The following seems to hang when running inside the deploymentScripts context.
+# Connect-AzAccount -UseDeviceAuthentication
 
 $DeploymentScriptOutputs = @{}
 $DeploymentScriptOutputs['text'] = $output
