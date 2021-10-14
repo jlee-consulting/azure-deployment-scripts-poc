0289f74d3d1bfb738917c0a67fe49fd84981ddf4
jlee-consulting <36266460+jlee-consulting@users.noreply.github.com>
2021-10-14 01:14:19 -0400

    Connect-AzAccount -UseDeviceAuthentication

--- azure-powershell/azure-powershell-v4.ps1
+++ azure-powershell/azure-powershell-v4.ps1
@@ -3,8 +3,10 @@ param([string] $subscriptionId)
 $output = 'subscriptionId: {0}' -f $subscriptionId
 Write-Output $output
 
-Connect-AzAccount -Subscription $subscriptionId
+## WARNING: Interactive authentication is not supported in this session, please run cmdlet 'Connect-AzAccount -UseDeviceAuthentication'.
+# Connect-AzAccount -Subscription $subscriptionId
 
+Connect-AzAccount -UseDeviceAuthentication
 
 $DeploymentScriptOutputs = @{}
 $DeploymentScriptOutputs['text'] = $output
